8379c64a6337a8062ebb748391a1a66d41a55e8b
winstxnhdw <winstxnhdw@gmail.com>
2022-12-19 23:42:37 +0800

    fix(camera): fix buffer len error

--- app/camera/__init__.py
+++ app/camera/__init__.py
@@ -1,24 +1,27 @@
 from threading import Thread
 
-from cv2 import VideoCapture
+from cv2 import VideoCapture, CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT
 from numpy import ndarray
 from typing_extensions import Self
 
-
 class Camera:
 
-    def __init__(self, capture_id: int=0):
+    def __init__(self, capture_id: int=0, width: int=1920, height: int=1080):
         
         self.capture_id = capture_id
+        self.width = width
+        self.height = height
         self.capture: VideoCapture
         self.capture_thread: Thread
-        self.buffer: ndarray
+        self.buffer: ndarray = []
         self.stop_capture = False
 
 
     def __enter__(self) -> Self:
 
         self.capture = VideoCapture(self.capture_id)
+        self.capture.set(CAP_PROP_FRAME_WIDTH, self.width)
+        self.capture.set(CAP_PROP_FRAME_HEIGHT, self.height)
 
         if not self.capture.isOpened():
             raise IOError("Unable to open device.")
